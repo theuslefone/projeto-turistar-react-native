a6f1b91031347da104464fd2cd6e449667b78620
Theuslefone <matheuskeeven22@gmail.com>
2021-11-29 16:27:29 -0300

    Referencia Map

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Map from '../src/components/Map';
+
 export default function HomeScreen() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Teste</Text>
+        <Map/>
       </View>
     );
   }
