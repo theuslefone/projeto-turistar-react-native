1452791af06ef72dc2a23505fe283f39d7c9065c
Theuslefone <matheuskeeven22@gmail.com>
2021-11-29 13:36:10 -0300

    Cria

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Teste</Text>
+      </View>
+    );
+  }
