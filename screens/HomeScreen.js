eb3298b218c0dbd4d2a3c44cb4cdb195030115e7
Theuslefone <matheuskeeven22@gmail.com>
2021-11-29 18:21:17 -0300

    Faz Tabulação

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import Map from '../src/components/Map';
+import Culinaria from '../src/tabs/Culinaria';
+import Cultura from '../src/tabs/Cultura';
+import Passeios from '../src/tabs/Passeios';
 
+const Tab = createMaterialTopTabNavigator();
 export default function HomeScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Teste</Text>
-        <Map/>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name="Culinaria" component={Culinaria} />
+        <Tab.Screen name="Cultura" component={Cultura} />
+        <Tab.Screen name="Passeios" component={Passeios} />
+      </Tab.Navigator>
     );
   }
+  
