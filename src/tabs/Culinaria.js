6555e432bfe0537b64e8f9a6233b4e53d73aa0e3
Theuslefone <matheuskeeven22@gmail.com>
2021-12-03 11:54:20 -0300

    Faz consumo heroku

--- src/tabs/Culinaria.js
+++ src/tabs/Culinaria.js
@@ -1,11 +1,44 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View,  ActivityIndicator, FlatList, } from 'react-native';
 import GoogleMap from '../components/GoogleMap';
+import { api } from '../../service/Heroku';
+
 
 export default function Culinaria() {
+    const [data, setData] = useState([]);
+    
+    const getCulinaria = async () =>{
+      try {
+        const response = await fetch(`${api}/rotas`);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    useEffect(() => {
+      getCulinaria()
+    }, []);
+
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <GoogleMap/>
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>
+            Inicio: {item.inicio},
+            {item.wayPoint1},
+            {item.wayPoint2},
+            {item.wayPoint3},
+            {item.wayPoint4},
+            {item.wayPoint5}, 
+            Fim: {item.fim} 
+            </Text>
+          )}
+        />
       </View>
     );
   }
