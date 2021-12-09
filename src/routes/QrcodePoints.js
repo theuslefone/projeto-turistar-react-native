f76bed8627ef38702d044daf9eb48d364a536778
Luan Rigaud <rigaudluanf@gmail.com>
2021-12-09 12:01:26 -0300

    cria Markers do mapa

--- src/routes/QrcodePoints.js
+++ src/routes/QrcodePoints.js
@@ -1,13 +1,52 @@
 import * as React from 'react';
 
-export const QrcodePoints = [
+export const marcoZero = 
     {
-        latitude: -8.062469625063406,
-        longitude: -34.87122195654575
-    },
-
+        latitude: -8.063139486093206,
+        longitude: -34.871117515874815
+    }
+export const  praçaDoArsenal = 
     {
         latitude: -8.061028117707304, 
         longitude: -34.87130268903499
     }
-]
+export const  torreMalakoff = 
+    {
+        latitude: -8.060759581517077, 
+        longitude: -34.87083255485408
+    }
+export const  casaraoMarcoZero = 
+    {
+        latitude: -8.06289069998735, 
+        longitude: -34.871510382007486
+    }
+export const  ruaBomJesus = 
+    {
+        latitude: -8.062366075749285, 
+        longitude: -34.871504168574106
+    }
+export const  leaoDoMestreNuca = 
+    {
+        latitude: -8.0578467915192, 
+        longitude: -34.86956789825603
+    }
+export const  caisDaLingueta = 
+    {
+        latitude: -8.061732859946618, 
+        longitude: -34.87047463555872
+    }
+export const  estatuaChicoScience = 
+    {
+        latitude: -8.064607752053636, 
+        longitude: -34.87288157756728
+    }
+export const  forteDoBrum = 
+    {
+        latitude: -8.053156651326754, 
+        longitude: -34.8710896157269
+    }
+export const  coracaoDoNordeste = 
+    {
+        latitude: -8.064656906358211, 
+        longitude: -34.8743306508345
+    }
