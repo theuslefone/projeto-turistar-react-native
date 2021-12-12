49bed54112ba85dddee16eecb8d39666681f85c4
Luan Rigaud <rigaudluanf@gmail.com>
2021-12-11 22:13:17 -0300

    atualiza modal

--- src/components/LocalCard.js
+++ src/components/LocalCard.js
@@ -1,20 +1,92 @@
-import * as React from 'react';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, Pressable, View, Image } from "react-native";
 
-//const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
-export default function LocalCard(props) {
-    
-    <Card>
-        <Card.Actions>
-            <Button>x</Button>
-        </Card.Actions>
-        <Card.Title title="{props.title}"/>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Content>
-            <List.Item
-            title="{'. ' ${props.Ponto}}"/>
-        </Card.Content>
-    </Card>
+const LocalCard = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>X</Text>
+            </Pressable>
+            <Text style={styles.modalText}>Marco Zero</Text>
+            <Image
+              style={{width: 360, height: 250}}
+              source={require('./marcoZero.jpeg')}
+            />
+            <Text style={styles.modalText}>Rota 1</Text>
+            <Text style={styles.modalText}>Rota 2</Text>
+            <Text style={styles.modalText}>Rota 3</Text>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </View>
+  );
+};
 
-}
+const styles = StyleSheet.create({
+  
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
+
+export default LocalCard.js;
